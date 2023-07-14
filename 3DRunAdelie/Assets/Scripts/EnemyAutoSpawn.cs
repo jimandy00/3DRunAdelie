1ba898886810aafbb7518a3e039bb9c1dfab1543
Jihwan Byeon <wkdghk24@gmail.com>
2023-07-14 15:37:44 +0900

    [create] Spawn enemy
    [create] Spawn enemy

---
+++ 3DRunAdelie/Assets/Scripts/EnemyAutoSpawn.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// enemy 자동 스폰
+
+// 시간이 흐르면
+// 해당 위치에
+// 적을 스폰하고
+// 내 위치 추적 or 아래로 떨어진다.
+
+public class EnemyAutoSpawn : MonoBehaviour
+{
+
+    // 현재 시간
+    float currentTime;
+
+    // 일정 시간
+    public float createTime = 1;
+
+    // 적 공장
+    public GameObject enemyFactory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 시작이 흐르다가
+        currentTime += Time.deltaTime;
+
+        // 만약 일정 시간이 되면
+        if (currentTime > createTime)
+        {
+            // 적 공장에서 적을 생성
+            GameObject enemy = Instantiate(enemyFactory);
+
+            // 적을 내 위치로 이동
+            enemy.transform.position = transform.position;
+
+            // 현재시간 0으로 초기화
+            currentTime = 0;
+        }
+    }
+}
